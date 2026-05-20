f74f83b8359a6192406e596a772d1e4ca95f5d49
Kasim Ghanchi <kasimghanchi672@gmail.com>
2026-05-20 09:59:45 +0530

    updated

---
+++ retrain_lstm.py
@@ -0,0 +1,9 @@
+import os, sys
+from src import config
+from src.data_loader import prepare_dataset
+from run_baselines import run_lstm
+
+config.get_device()
+data_prepared = prepare_dataset(config.DATASETS['METR-LA']['path'])
+mean, std = data_prepared['mean'], data_prepared['std']
+run_lstm(data_prepared, 'METR-LA', mean, std)
